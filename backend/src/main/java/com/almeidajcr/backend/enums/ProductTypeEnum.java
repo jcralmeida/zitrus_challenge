f1dc66db2e8bfcccd5f1b11213840c8ffc33a48b
Jose Almeida <almeidajcr90@gmail.com>
2022-09-28 13:23:42 -0300

    [Backend] Add the ProductTypeEnum class

---
+++ backend/src/main/java/com/almeidajcr/backend/enums/ProductTypeEnum.java
@@ -0,0 +1,12 @@
+package com.almeidajcr.backend.enums;
+
+import com.almeidajcr.backend.entity.Product;
+
+/**
+ * Define the possible type of the {@link Product}.
+ */
+public enum ProductTypeEnum {
+  ELETRONICO,
+  ELETRODOMESTICO,
+  MOVEL
+}
