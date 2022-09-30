7c10240e0d6d8c4af43f610397804d020ee409a3
Jose Almeida <almeidajcr90@gmail.com>
2022-09-30 08:44:16 -0300

    [Backend] Add annotation Response Status to the NotFoundException

--- backend/src/main/java/com/almeidajcr/backend/exception/NotFoundException.java
+++ backend/src/main/java/com/almeidajcr/backend/exception/NotFoundException.java
@@ -1,8 +1,12 @@
 package com.almeidajcr.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Custom exception to be thrown when an object is not found.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
   private static final long serialVersionUID = 1L;
 
