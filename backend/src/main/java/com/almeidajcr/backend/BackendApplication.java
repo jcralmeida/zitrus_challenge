8478d7feb3cbf5850e5eac057a4257c45633517b
Jose Almeida <almeidajcr90@gmail.com>
2022-10-02 16:33:17 -0300

    [Backend] Handle CORs request

--- backend/src/main/java/com/almeidajcr/backend/BackendApplication.java
+++ backend/src/main/java/com/almeidajcr/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.almeidajcr.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * The main class for the rest application.
@@ -13,4 +16,21 @@ public class BackendApplication {
     SpringApplication.run(BackendApplication.class, args);
   }
 
+  /**
+   * Configure CORs for the application.
+   *
+   * @return a {@link WebMvcConfigurer}.
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*");
+      }
+    };
+  }
 }
